92a81fa99fef5d045e7da487267654edd65e8156
unixk <unixk@m5-multitronic-unit>
2025-04-15 22:20:11 -0400

    Initial Commit

---
+++ src/test/java/org/kcs/demo/springboot/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kcs.demo.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
