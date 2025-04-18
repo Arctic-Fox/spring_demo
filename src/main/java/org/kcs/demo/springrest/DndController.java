c31361102647c09d7376fcc32ac827e928caf511
unixk <unixk@m5-multitronic-unit>
2025-04-18 15:40:38 -0400

    Remove unneeded classes and packages.  Rename DndController

--- src/main/java/org/kcs/demo/springrest/DndController.java
+++ src/main/java/org/kcs/demo/springrest/DndController.java
@@ -9,21 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class DndController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
-
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(defaultValue = "World") String name) {
-		return String.format("Hello %s! You rock my face off!  By the way...%s", name, simpleTemplate());
-	}
-	
 	@GetMapping("/spells/all")
 	public String spells() {
 		return DndRestClient.getEntity(Category.SPELLS);
@@ -42,9 +29,5 @@ public class GreetingController {
 	@GetMapping("/monster")
 	public String monster(@RequestParam(defaultValue = "") String name) {
 		return DndRestClient.getEntity(Category.MONSTERS, name);
-	}
-
-	private String simpleTemplate() {
-		return "<b>This is a silly little template.</b>";
 	}
 }
--- src/main/java/org/kcs/demo/springrest/Greeting.java
+++
@@ -1,3 +0,0 @@
-package org.kcs.demo.springrest;
-
-public record Greeting(long id, String content) { }
